5e8962f0b6d0cd0c15af656b4d8e7028f2b422fc
jibin <jibinjolly32@gmail.com>
2024-11-02 18:29:41 +0530

    first

---
+++ build.gradle.kts
@@ -0,0 +1,10 @@
+buildscript {
+    dependencies {
+        classpath (libs.google.services)
+    }
+}// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    // Other plugins
+}
